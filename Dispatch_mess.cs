c2d33f74bf27f6e5c75c0466118c0579e02bf8e8
Ilya <ilyademosmirnov@gmail.com>
2019-08-10 18:43:23 +0300

    0.02

--- Dispatch_mess.cs
+++ Dispatch_mess.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing.Drawing2D;
+using System.Drawing;
 
 namespace OOP_drow
 {
-    class Dispatch_mess
+    class Dispatch_mess : Line
     {
+
+        public override void Draw(Graphics gr)
+        {
+            Pen Pe = new Pen(Line_color);
+            Pe.Width = Line_width;
+            Pe.EndCap = LineCap.Custom;
+            Pe.CustomEndCap = new AdjustableArrowCap(3f, 3f);
+            gr.DrawLine(Pe, Basic_points[0], Basic_points[1]);
+        }
+
     }
 }
--- Return_mess.cs
+++ Return_mess.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Drawing.Drawing2D;
+using System.Drawing;
 
 namespace OOP_drow
 {
-    class Return_mess
+    class Return_mess : Line
     {
+        public override void Draw(Graphics gr)
+        {
+            Pen Pe = new Pen(Line_color);
+            Pe.DashStyle = DashStyle.Dash;
+            Pe.Width = Line_width;
+            Pe.EndCap = System.Drawing.Drawing2D.LineCap.Custom;
+            Pe.CustomEndCap = new AdjustableArrowCap(3f, 3f, false);
+            gr.DrawLine(Pe, Basic_points[0], Basic_points[1]);
+        }
     }
 }
