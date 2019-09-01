8f0bbeff76c569994954a7ff6becf094afa1f403
YuriSilenok <yuri.silenok@gmail.com>
2019-09-01 03:32:35 +0300

    концы линий переделаны через наследование

---
+++ KSU.Visio.Lib/Cap/AsynchronousMessageCap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSU.Visio.Lib.Cap
+{
+    public class AsynchronousMessageCap : LineCapBase
+    {
+        public AsynchronousMessageCap() : base()
+        {
+            strokePath.AddLine(0, 0, -3, -10);
+            strokePath.AddLine(0, 0, 3, -10);
+        }
+    }
+}
