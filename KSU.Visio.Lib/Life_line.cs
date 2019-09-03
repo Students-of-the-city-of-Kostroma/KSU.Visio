d895c5f832cd82365c695dec0ada6132d681a4ec
Ilya <ilyademosmirnov@gmail.com>
2019-09-03 20:58:18 +0300

    попыткадопилить1

--- KSU.Visio.Lib/Life_line.cs
+++ KSU.Visio.Lib/Life_line.cs
@@ -7,60 +7,45 @@ using System.Drawing;
 
 namespace KSU.Visio.Lib
 {
-    public class Life_line : Rectangle_object
+    public class Life_line : Figure
     {
+        public Life_line(Point location, Size size) : base(location, size)
+        {
+
+        }
+
+        public override Figure Clone()
+        {
+            Figure figure = new Life_line(Location, Size);
+            figure.Selected = Selected;
+            return figure;
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            base.Draw(gr);
+            Pen Pe = (Pen)pen.Clone();
+            gr.DrawRectangle(pen, new Rectangle(Location, Size));
+            Pen.DashStyle = DashStyle.Dash;
+            Pen.Color = Pe.Color;
+            //SortLine();
+            gr.DrawLine(Pen, Line_ends[0], Line_ends[1]);
+        }
+
         int Line_length = 50;
         static Pen Pen = new Pen(Brushes.Black, 3);
         Point[] Line_ends = new Point[2];
         bool Edit_line = false;//Определает, нужно ли изменять длинну линии
 
-        public Life_line(Point location, Size size)
-            : base(location, size) { }
-
-        override public void Draw(Graphics gr)
-        {
-            Pen Pe = (Pen)pen.Clone();
-            gr.DrawRectangle(pen, Location.X+Size.Width, Location.Y, Location.X - Location.X+Size.Width, Location.Y - Location.Y);
-            Pen.DashStyle = DashStyle.Dash;
-            Pen.Color = Pe.Color;
-            SortLine(); 
-            gr.DrawLine(Pen, Line_ends[0], Line_ends[1]);
-
-
-        }
-        private void SortLine()
-        {
+       
+    
+        //private void SortLine()
+        //{
             
-            int mid = (Location.X + Location.X+Location.X+Size.Width) / 2;//середина нижней линии прямоугольника (из этой точки выходит линия жизни) 
-            Line_ends[0] = new Point(mid, Location.Y);
-            Line_ends[1] = new Point(mid, Location.Y + Line_length);
-        }
+        //    int mid = (Location.X + Location.X+Location.X+Size.Width) / 2;//середина нижней линии прямоугольника (из этой точки выходит линия жизни) 
+        //    Line_ends[0] = new Point(mid, Location.Y);
+        //    Line_ends[1] = new Point(mid, Location.Y + Line_length);
+        //}
 
-        public bool Hit_testing_line(Point Point)
-        {
-            SortLine();
-            //уравнение окружности с центром в точке клика (находим растояние до ближайшей точки данного объекта)
-            double d1 = Math.Sqrt(Math.Pow(Point.X - (this as Life_line).Line_ends[1].X, 2) + Math.Pow(Point.Y - (this as Life_line).Line_ends[1].Y, 2));
-            int r = 8;//радиус. Если расстояние d1 меньше радиуса, то цепляем конец для перемещения
-            if (d1 <= r)
-            {
-                Edit_line = true;
-                return true;
-            }
-
-            return false;
-        }
-
-        public void Shift_line(Point K)
-        {
-            if (K.Y < Line_ends[0].Y + 10)//Если предлагаемое новое расстояние меньше десяти пикселей, то ничего не делаем
-                return;
-            int t = K.Y - Line_ends[1].Y;//Находим разницу между старым концом линии жизни и новым, а затем меняем расстояние
-            Line_ends[1] = K;
-            if (Line_length > t)
-                Line_length += t;
-            else Line_length -= t;
-
-        }
     }
 }
--- KSU.Visio.Lib/White_rectangle.cs
+++ KSU.Visio.Lib/White_rectangle.cs
@@ -6,15 +6,28 @@ using System.Drawing;
 
 namespace KSU.Visio.Lib
 {
-    public class White_rectangle : Rectangle_object
+    public class White_rectangle : Figure
     {
-        SolidBrush Brush = new SolidBrush(Color.White);
-        public White_rectangle(Point location, Size size)
-            : base(location, size) { }
-        override public void Draw(Graphics gr)
+        public White_rectangle(Point location, Size size) : base(location, size)
         {
-            gr.FillRectangle(Brush, Location.X + Size.Width, Location.Y, Location.X - Location.X + Size.Width, Location.Y - Location.Y);
-            gr.DrawRectangle(pen, Location.X + Size.Width, Location.Y, Location.X - Location.X + Size.Width, Location.Y - Location.Y);
+
         }
+
+        public override Figure Clone()
+        {
+            Figure figure = new White_rectangle(Location, Size);
+            figure.Selected = Selected;
+            return figure;
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            base.Draw(gr);
+            gr.FillRectangle(Brush, Location.X + Size.Width, Location.Y, Location.X - Location.X + Size.Width, Location.Y - Location.Y);
+            gr.DrawRectangle(pen, new Rectangle(Location, Size));
+        }
+
+        SolidBrush Brush = new SolidBrush(Color.White); //ЛИШНЕЕ, НО НЕ ЗНАЮ КАК ИСПРАВИТЬ НА ПЕРО
+       
     }
 }
