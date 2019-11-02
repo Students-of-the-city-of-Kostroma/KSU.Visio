d1cb980c26bb8e53a07a3fd673d60371971d9826
YuriSilenok <yuri.silenok@gmail.com>
2019-11-02 18:46:10 +0300

    new file

---
+++ KSU.Visio.Lib/StateDiagram/Condition.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace KSU.Visio.Lib.StateDiagram
+{
+    /// <summary>
+    /// Состояние
+    /// </summary>
+    public class Condition : SDBase
+    {
+        public bool Active { get; set; }
+        public List<Condition> Conditions { get; set; }
+        public List<Transfer> Transfers { get; set; }
+        public Condition(XmlNode stateXML) : base (stateXML)
+        {
+            Conditions = new List<Condition>();
+            Transfers = new List<Transfer>();
+        }
+
+        public Condition(Point location, Size size) : base(location, size)
+        {
+            Conditions = new List<Condition>();
+            Transfers = new List<Transfer>();
+        }
+
+
+        //public Condition Owner { get; set; }
+        public bool Status { get; set; }
+        
+
+        public override XmlNode ToXml(XmlDocument xml, XmlNode stateXML)
+        {
+            XmlNode figureXml = base.ToXml(xml, stateXML);
+
+            XmlAttribute activeAttr = xml.CreateAttribute("active");
+            activeAttr.Value = Active.ToString();
+            figureXml.Attributes.Append(activeAttr);
+
+            foreach (Condition state in Conditions)
+                state.ToXml(xml, figureXml);
+
+            foreach (Transfer transfer in Transfers)
+                transfer.ToXml(xml, figureXml);
+
+            return figureXml;
+        }
+
+        public override void Draw(Graphics gr)
+        {
+            base.Draw(gr);
+
+            ///диаметр
+            int r = ((Size.Width < Size.Height) ? Size.Width : Size.Height) / 10;
+            r = (r < 5) ? 5 : r;
+
+            ///h = horisontal, v - vertical
+            ///l - left, r - right
+            ///t - top, b - bottom
+
+            Point hlt = new Point(location.X + r, location.Y);
+            Point hrt = new Point(location.X + size.Width - r, hlt.Y);
+
+            Point hlb = new Point(hlt.X, location.Y + size.Height);
+            Point hrb = new Point(hrt.X, hlb.Y);
+
+            Point vlt = new Point(location.X, location.Y + r);
+            Point vlb = new Point(vlt.X, location.Y + size.Height - r);
+
+            Point vrt = new Point(location.X + size.Width, vlt.Y);
+            Point vrb = new Point(vrt.X, vlb.Y);
+
+
+            gr.DrawLine(pen, hlt, hrt);
+            gr.DrawLine(pen, hlb, hrb);
+
+            gr.DrawLine(pen, vlt, vlb);
+            gr.DrawLine(pen, vrt, vrb);
+
+            int d = r * 2;
+
+            Point locLT = location;
+            Point locLB = new Point(locLT.X, vlb.Y - r);
+            Point locRT = new Point(hrt.X - r, locLT.Y);
+            Point locRB = new Point(locRT.X, locLB.Y);
+            Size sizeArc = new Size(d, d);
+
+            gr.DrawArc(pen, new Rectangle(locLT, sizeArc), 180f, 90f);  //LT
+            gr.DrawArc(pen, new Rectangle(locLB, sizeArc), 90f, 90f);   //LB
+            gr.DrawArc(pen, new Rectangle(locRT, sizeArc), 270f, 90f);  //RT
+            gr.DrawArc(pen, new Rectangle(locRB, sizeArc), 0f, 90f);    //RB
+
+            RectangleF recF = new RectangleF(location, size);
+            StringFormat drawFormat = new StringFormat();
+
+            gr.DrawString(Name, font, brush, recF, drawFormat);
+        }
+    }
+}
+
